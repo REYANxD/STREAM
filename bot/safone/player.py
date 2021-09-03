1ab9d7f7857c4933ad65cd7c792323741d60d73b
VASU-XD <79446681+VASU-XD@users.noreply.github.com>
2021-09-03 11:13:27 +0530

    Update player.py

--- bot/safone/player.py
+++ bot/safone/player.py
@@ -146,7 +146,7 @@ admincmds=["stream", "pause", "resume", "endstream", f"stream@{USERNAME}", f"pau
 
 @Client.on_message(filters.command(admincmds) & ~filters.user(ADMINS) & (filters.chat(CHAT_ID) | filters.private))
 async def notforu(_, m: Message):
-    k = await m.reply_sticker("CAACAgUAAxkBAAEBpyZhF4R-ZbS5HUrOxI_MSQ10hQt65QACcAMAApOsoVSPUT5eqj5H0h4E")
+    k = await m.reply_sticker("CAACAgEAAxkBAAELlPVhMbW5jYcVmgGzkI2QUdkLaUHVHAADAQACqx6hRNslQs_kBc9VIAQ")
     await sleep(5)
     await k.delete()
     try:
