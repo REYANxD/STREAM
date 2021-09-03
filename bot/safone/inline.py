83863f4259b48b1c95cff0365e98c3ab0af88c6d
VASU-XD <79446681+VASU-XD@users.noreply.github.com>
2021-09-03 11:03:51 +0530

    Update inline.py

--- bot/safone/inline.py
+++ bot/safone/inline.py
@@ -27,12 +27,9 @@ REPLY_MESSAGE = Config.REPLY_MESSAGE
 
 buttons = [
             [
-                InlineKeyboardButton("CHANNEL", url="https://t.me/AsmSafone"),
-                InlineKeyboardButton("SUPPORT", url="https://t.me/SafoTheBot"),
+                InlineKeyboardButton("CHANNEL", url="https://t.me/TrixieMusic"),
+                InlineKeyboardButton("SUPPORT", url="https://t.me/IndicSupport"),
             ],
-            [
-                InlineKeyboardButton("🤖 MAKE YOUR OWN BOT 🤖", url="https://heroku.com/deploy?template=https://github.com/AsmSafone/VideoPlayerBot"),
-            ]
          ]
 
 @Client.on_inline_query()
@@ -42,7 +39,7 @@ async def search(client, query):
         answers.append(
             InlineQueryResultArticle(
                 title="Deploy Own Video Player Bot",
-                input_message_content=InputTextMessageContent(f"{REPLY_MESSAGE}\n\n<b>© Powered By : \n@AsmSafone | @SafoTheBot 👑</b>", disable_web_page_preview=True),
+                input_message_content=InputTextMessageContent(f"{REPLY_MESSAGE}\n\n<b>© Powered By : \n@TrixieMusic | @xxvasu 👑</b>", disable_web_page_preview=True),
                 reply_markup=InlineKeyboardMarkup(buttons)
                 )
             )
